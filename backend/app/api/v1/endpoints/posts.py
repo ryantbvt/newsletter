be86ef82526505d0911163874dbc9ffcf671dd0f
Ryan Bui <ryan.bui@gmail.com>
2025-03-09 11:24:28 -0700

    add get post

--- backend/app/api/v1/endpoints/posts.py
+++ backend/app/api/v1/endpoints/posts.py
@@ -57,8 +57,36 @@ async def create_post(post: CreatePost, db: Session = Depends(get_db)):
     # Add new post to db
     db.add(new_post)
     db.commit()
-    db.refresh(new_post) 
+    db.refresh(new_post)
 
     logger.info(f"New post created. ID: {new_post.id}")
 
     return new_post
+
+@router.get("/{id}", response_model=CreatePost, status_code=200)
+async def fetch_post(id: int, db: Session = Depends(get_db)):
+    '''
+    Description: Fetch post by id.
+
+    Args:
+        id (int): post id
+        db (Session): Database session dependency
+
+    Return:
+        post (CreatePost): the post found in db
+
+    Exceptions:
+        404: when post is not found in db
+    '''
+    logger.info(f"Attempting to fetch post id: {id}")
+
+    # Query db
+    post = db.query(Post).filter(Post.id == id).first()
+
+    if post is None:
+        logger.warning(f"Post {id} not found.")
+        raise HTTPException(status_code=404, detail=f"Post {id} not found.")
+    
+    logger.info(f"Found post {id}, returning...")
+
+    return post
